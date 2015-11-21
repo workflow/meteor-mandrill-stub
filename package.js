6f01bbf5e49435e4f1655d00249c16746ee30efb
Workflow <florian.peter@gmx.at>
2015-11-21 13:46:35 +0100

    Specifying dependency version numbers.

--- package.js
+++ package.js
@@ -15,7 +15,7 @@ Package.onUse(function(api) {
   api.versionsFrom('1.2.1');
 
   api.use('ecmascript');
-  api.use('wylio:mandrill');
+  api.use('wylio:mandrill@1.0.0');
 
   api.addFiles('server/mandrill-stub.js', 'server');
 });
